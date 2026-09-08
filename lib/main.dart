b817f3902ebb608504b72e930fb89d46d4685c27
Sabarinath P <sabarinath@Sabarinaths-MacBook-Air.local>
2026-09-08 09:26:57 +0530

    product retunr values

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,11 @@ import 'package:get/get.dart';
 import 'package:kinfox_biller/LoginScreen/Service/AuthController.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-String authbaseUrl = (true)
+String authbaseUrl = (false)
     ? "https://api.kingfox.palqar.cloud/v1"
     : "https://api.kingfoxclothing.com/v1";
 
-String baseUrl = (true)
+String baseUrl = (false)
     ? "https://api.kingfox.palqar.cloud/v1/protected/kingfox"
     : "https://api.kingfoxclothing.com/v1/protected/kingfox";
 String? accessToken;
